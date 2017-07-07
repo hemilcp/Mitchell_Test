e9653dad4b2079d490a7e006a95fb3c9f7e90f9c
Hemil Parmar <hemilcp@gmail.com>
2017-07-06 19:39:08 -0700

    Vehicle Store Rest API

---
+++ VehicleStore/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using VehicleStore.DAO;
+using VehicleStore.Models;
+
+namespace VehicleStore.Controllers
+{
+    [EnableCors(origins: "*", headers:"*", methods:"*")]
+    public class HomeController : ApiController
+    {
+
+        Models.Vehicle[] vehicles = new Vehicle[]
+        {
+            new Vehicle { Id = 1, Year = 2014, Make = "Tesla", Model = "Model S"},
+            new Vehicle { Id = 2, Year = 2013, Make = "Lambourghini", Model = "Avantador"},
+            new Vehicle { Id = 3, Year = 2004, Make = "Suzuki", Model = "Maruti 800"},
+            new Vehicle { Id = 4, Year = 2014, Make = "Honda", Model = "Accent"},
+            new Vehicle { Id = 5, Year = 2017, Make = "Hundai", Model = "VeganR"},
+            new Vehicle { Id = 6, Year = 2012, Make = "Tata", Model = "Nano"},
+        };
+
+        DAO.TempMemory db = new TempMemory();
+
+        [HttpPost]
+        public string AddVehicleDetails(Vehicle input)
+        {
+            var check = db.addNewVehicleDetails(input);
+            if (check) return "Vehicle Details saved successfully!";
+            else return "A record with same Id exists!";
+        }
+        [HttpGet]
+        public IEnumerable<Vehicle> GetAllVehiclesDetails()
+        {
+            return db.getAllVehicleList();
+
+        }
+
+        public IHttpActionResult GetVehicleDetails(int id)
+        {
+            var vehicle = db.getVehicleDetails(id);
+            if (vehicle == null)
+                return NotFound();
+            return Ok(vehicle);
+        }
+
+        [HttpDelete]
+        public string DeleteVehicleDetails(Vehicle input)
+        {
+            var check = db.deleteVehicleDetails(input);
+            if (check) return "Vehicle details deleted successfully!";
+            else return "No record exists with such details!";   
+        }
+
+        [HttpPut]
+        public string UpdateVehicleDetails(string Name, String Id)
+        {
+            return "Vehicle details Updated with Name " + Name + " and Id " + Id;
+
+        }
+
+    }
+}
---
+++ VehicleStore/DAO/TempMemory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VehicleStore.Models;
+
+namespace VehicleStore.DAO
+{
+    public class TempMemory
+    {
+        static List<Vehicle> VehicleDB = new List<Vehicle>();
+
+
+         public TempMemory()
+        {
+      //      VehicleDB = new List<Vehicle>();
+     //       Vehicle v1 = new Vehicle { Id = 1, Year = 2013, Make = "Honda", Model = "City" };
+     //       Vehicle v2 = new Vehicle { Id = 2, Year = 2016, Make = "Honda", Model = "Accent" };
+
+         //   VehicleDB.Add(v1);
+          //  VehicleDB.Add(v2);
+        }
+
+        //get all vehicle records 
+        public List<Vehicle> getAllVehicleList()
+        {
+            return TempMemory.VehicleDB;
+        }
+
+        //get the details of the records of vehicle by id
+        public Vehicle getVehicleDetails(int id)
+        {
+            var vehicle = VehicleDB.SingleOrDefault(item => item.Id == id);
+            return vehicle;
+        }
+
+        //Add new vehicle record
+        public bool addNewVehicleDetails(Vehicle input)
+        {
+            bool flag = false;
+            //before adding vehicle record, check if there is any existing record
+            if (!checkIfVehicleExists(input.Id))
+            {
+                TempMemory.VehicleDB.Add(input);
+                return true;
+            }
+            return flag;
+        }
+
+        //Delete a vehicle record
+        public bool deleteVehicleDetails(Vehicle input)
+        {
+            //Get the vehicle record to delete
+            var vehicle = VehicleDB.Single(item => item.Id == input.Id);
+            if (vehicle != null) { VehicleDB.Remove(vehicle); return true; }
+            else return false;
+            
+        }
+
+        //Check if any vehicle record exists with the same ID. Return true if exists.
+        public bool checkIfVehicleExists(int id)
+        {
+            var vehicle = VehicleDB.Exists(item => item.Id == id);
+            if (vehicle) return true;
+            else return false;
+        }
+
+    }
+}
